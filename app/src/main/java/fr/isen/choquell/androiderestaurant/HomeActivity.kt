ae1a551a970a3575d97f7a8b7b246cec920ad310
thomaschok <thomas.choquell@isen.yncrea.fr>
2023-01-16 11:19:36 +0100

    Création_premier_commit

---
+++ app/src/main/java/fr/isen/choquell/androiderestaurant/HomeActivity.kt
@@ -0,0 +1,34 @@
+package fr.isen.choquell.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    val action = findViewById<Button>(R.id.buttonEntrée_affichage)
+        action.setOnClickListener() {
+            Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
+        }
+
+
+        val action2= findViewById<Button>(R.id.buttonPlat_affichage)
+        action2.setOnClickListener() {
+            Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
+        }
+
+        val action3 = findViewById<Button>(R.id.buttonDessert_affichage)
+        action3.setOnClickListener() {
+            Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
+
+
