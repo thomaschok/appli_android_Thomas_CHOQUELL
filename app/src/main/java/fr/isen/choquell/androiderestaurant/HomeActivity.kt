5c96e699ad18ae0f408e5830b9bea5b5aeaf1d38
thomaschok <thomas.choquell@isen.yncrea.fr>
2023-01-16 13:09:33 +0100

    Création_premier_commit

--- app/src/main/java/fr/isen/choquell/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/choquell/androiderestaurant/HomeActivity.kt
@@ -1,10 +1,14 @@
 package fr.isen.choquell.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.id.button1
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +20,19 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, "Creeper, OH MAN!!!", Toast.LENGTH_SHORT).show();
         }
 
-
         val action2= findViewById<Button>(R.id.buttonPlat_affichage)
-        action2.setOnClickListener() {
-            Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
+        action2.setOnClickListener {
+            val intent = Intent(applicationContext, PlatActivity::class.java)
+            startActivity(intent);
         }
 
-        val action3 = findViewById<Button>(R.id.buttonDessert_affichage)
+     /*   val action3= findViewById<Button>(R.id.buttonPlat_affichage)
         action3.setOnClickListener() {
+            Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
+        }
+*/
+        val action4 = findViewById<Button>(R.id.buttonDessert_affichage)
+        action4.setOnClickListener() {
             Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
         }
     }
