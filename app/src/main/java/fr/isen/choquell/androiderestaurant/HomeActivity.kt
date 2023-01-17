3a6ea7cdfec5ef1c7eeb92875a46feaa498b4338
thomaschok <thomas.choquell@isen.yncrea.fr>
2023-01-17 08:43:01 +0100

    Création_Deuxième_commit

--- app/src/main/java/fr/isen/choquell/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/choquell/androiderestaurant/HomeActivity.kt
@@ -1,40 +1,76 @@
 package fr.isen.choquell.androiderestaurant
 
-import android.R.id.button1
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.view.Menu
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import fr.isen.choquell.androiderestaurant.databinding.ActivityHomeBinding
 
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "World"
+
+
+        /*
         val action = findViewById<Button>(R.id.buttonEntrée_affichage)
         action.setOnClickListener() {
             Toast.makeText(applicationContext, "Creeper, OH MAN!!!", Toast.LENGTH_SHORT).show();
         }
+        */
 
-        val action2= findViewById<Button>(R.id.buttonPlat_affichage)
-        action2.setOnClickListener {
-            val intent = Intent(applicationContext, PlatActivity::class.java)
-            startActivity(intent);
+        //val action1= findViewById<Button>(R.id.buttonEntrée_affichage)
+        binding.buttonStarteraffichage.setOnClickListener {
+
+            Log.d("TAG", "Home est delete")
+
+            val intent = Intent(this@HomeActivity, PlatActivity::class.java)
+            intent.putExtra("samplename", "Nether")
+            startActivity(intent)
         }
 
-     /*   val action3= findViewById<Button>(R.id.buttonPlat_affichage)
+
+        binding.buttonPlataffichage.setOnClickListener {
+            val intent= Intent(this@HomeActivity, PlatActivity::class.java)
+            intent.putExtra("samplename", "Mineshaft")
+            startActivity(intent)
+        }
+
+
+        binding.buttonDessertaffichage.setOnClickListener {
+            val intent= Intent(this@HomeActivity, PlatActivity::class.java)
+            intent.putExtra("samplename", "Ender")
+            startActivity(intent)
+        }
+
+
+
+        /*val action3= findViewById<Button>(R.id.buttonPlat_affichage)
         action3.setOnClickListener() {
             Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
         }
-*/
-        val action4 = findViewById<Button>(R.id.buttonDessert_affichage)
+            */
+
+        /* val action4 = findViewById<Button>(R.id.buttonDessert_affichage)
         action4.setOnClickListener() {
             Snackbar.make(it, "Creeper, OH MAN!!!", Snackbar.LENGTH_SHORT).show()
         }
+
+         */
+
     }
 
 
--- app/src/main/java/fr/isen/choquell/androiderestaurant/PlatActivity.kt
+++ app/src/main/java/fr/isen/choquell/androiderestaurant/PlatActivity.kt
@@ -1,11 +1,55 @@
 package fr.isen.choquell.androiderestaurant
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import fr.isen.choquell.androiderestaurant.databinding.ActivityPlatBinding
+
 
 class PlatActivity : AppCompatActivity() {
+    private val listPlat = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
+    private lateinit var binding: ActivityPlatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_plat)
+        setContentView(binding.root)
+
+        val salut = intent.getStringExtra("samplename")
+
+        val actionBar = supportActionBar
+        actionBar?.title = salut
+
+        val recyclerView: RecyclerView = findViewById(R.id.mealList)
+        customAdapter = CustomAdapter(prepareItems())
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+
+
+
+    }
+
+    private fun prepareItems() : List<String> {
+        val itemsList = arrayListOf<String>()
+        itemsList.add("Item 1")
+        itemsList.add("Item 2")
+        itemsList.add("Item 3")
+        itemsList.add("Item 4")
+        itemsList.add("Item 5")
+        itemsList.add("Item 6")
+        itemsList.add("Item 7")
+        itemsList.add("Item 8")
+        itemsList.add("Item 9")
+        itemsList.add("Item 10")
+        itemsList.add("Item 11")
+        itemsList.add("Item 12")
+        itemsList.add("Item 13")
+        return itemsList
     }
 }
+
+
+
